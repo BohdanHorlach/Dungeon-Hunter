3421410abff75eac954be35568c937d242e680df
Bohdan <bogdangorlac@gmail.com>
2024-02-13 20:58:11 +0200

    Update EnemyAnimatorHandler.cs

--- Assets/Scripts/Enemy/EnemyAnimatorHandler.cs
+++ Assets/Scripts/Enemy/EnemyAnimatorHandler.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 public class EnemyAnimatorHandler : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform mainTransform;
     [SerializeField] private HealthHandler helthHandler;
+
+    private Vector2 previousPosition;
+    private bool isFacingRight = true;
+
+
+    private void Start()
+    {
+        previousPosition = mainTransform.position;
+    }
+
+
 
     private void OnEnable()
     {
@@ -18,6 +30,40 @@ public class EnemyAnimatorHandler : MonoBehaviour
         helthHandler.DamageReceived -= TakeDamage;
         helthHandler.Death -= Death;
     }
+
+
+    private void Update()
+    {
+        SetWalkAnimation();
+    }
+
+
+    private void SetWalkAnimation()
+    {
+        Vector2 actualPosition = mainTransform.position;
+        bool isWalk = actualPosition.Equals(previousPosition) == false;
+
+        SetFlip(actualPosition.x - previousPosition.x);
+
+        animator.SetBool("isWalk", isWalk);
+        previousPosition = actualPosition;
+    }
+
+
+    private void SetFlip(float diferenceFromX)
+    {
+        if (diferenceFromX > 0.0f && isFacingRight == false || diferenceFromX < 0.0f && isFacingRight == true)
+            Flip();
+    }
+
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        mainTransform.Rotate(0f, 180f, 0f);
+    }
+
 
 
     private void TakeDamage()
